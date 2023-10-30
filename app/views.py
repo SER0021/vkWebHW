c20f0a2c4e14ce5e9c4c25e815ade4e87ecc43bb
SER0021 <ser00021@yandex.ru>
2023-10-30 18:40:38 +0300

    add django templates and paginator

--- app/views.py
+++ app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
-# Create your views here.
+
+QUESTIONS = [
+        {
+            'id': i,
+            'title': f'Question {i}',
+            'content': f'Long lorem ipsum {i}'
+        } for i in range(10)
+    ]
+
+def paginate(objects, page, per_page=3):
+    paginator = Paginator(objects, per_page)
+    return paginator.page(page)
+
+def index(request):
+    page = request.GET.get('page', 1)
+    return render(request, 'index.html', {'questions': paginate(QUESTIONS, page)})
+
+
+def question(request, question_id):
+    item = QUESTIONS[question_id]
+    return render(request, 'question.html', {'question': item})
--- askme_vasilev/urls.py
+++ askme_vasilev/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
+
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('question/<int:question_id>', views.question, name='question'),
     path('admin/', admin.site.urls),
 ]
